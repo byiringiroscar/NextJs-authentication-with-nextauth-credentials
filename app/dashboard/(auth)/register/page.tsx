6c620d3d0960c54b5b6482f093b5d4f753574a0d
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-11 12:20:53 +0200

    Configure register form

--- app/dashboard/(auth)/register/page.tsx
+++ app/dashboard/(auth)/register/page.tsx
@@ -1,14 +1,31 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 const Register = () => {
+  const [info, setInfo] = useState({
+    username: "",
+    email: "",
+    password: "",
+  })
+
+  const handleInput = (e: any) => {
+    setInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+
+  }
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(info)
+
+  }
+
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
         <div className='w-1/3'>
             <h1 className='text-4xl font-bold mb-6'>Register</h1>
-            <form className='flex flex-col'>
-            <input className='border border-gray-400 mb-2 p-2' type='text' placeholder='Username' />
-            <input className='border border-gray-400 mb-2 p-2' type='email' placeholder='Email' />
-            <input className='border border-gray-400 mb-2 p-2' type='password' placeholder='Password' />
+            <form className='flex flex-col' onSubmit={handleSubmit}>
+            <input className='border border-gray-400 mb-2 p-2' name='username' type='text' placeholder='Username' onChange={(e) => handleInput(e)} />
+            <input className='border border-gray-400 mb-2 p-2' type='email' name='email' placeholder='Email' onChange={(e) => handleInput(e)} />
+            <input className='border border-gray-400 mb-2 p-2' type='password' name='password' placeholder='Password' onChange={(e) => handleInput(e)} />
             <button className='border border-gray-400 p-2'>Register</button>
             </form>
         </div>
