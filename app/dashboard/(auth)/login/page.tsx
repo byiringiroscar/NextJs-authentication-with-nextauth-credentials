2bec4188a58a813bf9f5cf0ea097bae66d942879
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-11 12:00:36 +0200

    Design authentication form

--- app/dashboard/(auth)/login/page.tsx
+++ app/dashboard/(auth)/login/page.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='w-screen h-screen flex justify-center items-center'>
+        <div className='w-1/3'>
+            <h1 className='text-4xl font-bold mb-6'>Login</h1>
+            <form className='flex flex-col'>
+            <input className='border border-gray-400 mb-2 p-2' type='text' placeholder='Email' />
+            <input className='border border-gray-400 mb-2 p-2' type='password' placeholder='Password' />
+            <button className='border border-gray-400 p-2'>Login</button>
+            </form>
+        </div>
+    </div>
   )
 }
 
--- app/dashboard/(auth)/register/page.tsx
+++ app/dashboard/(auth)/register/page.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='w-screen h-screen flex justify-center items-center'>
+        <div className='w-1/3'>
+            <h1 className='text-4xl font-bold mb-6'>Register</h1>
+            <form className='flex flex-col'>
+            <input className='border border-gray-400 mb-2 p-2' type='text' placeholder='Username' />
+            <input className='border border-gray-400 mb-2 p-2' type='email' placeholder='Email' />
+            <input className='border border-gray-400 mb-2 p-2' type='password' placeholder='Password' />
+            <button className='border border-gray-400 p-2'>Register</button>
+            </form>
+        </div>
+    </div>
   )
 }
 
