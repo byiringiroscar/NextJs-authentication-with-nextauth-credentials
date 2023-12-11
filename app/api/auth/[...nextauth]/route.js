7f6a7d1bddf0f92bf9c26d4ccaf5c801c4ba9456
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-12 00:54:32 +0200

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,25 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import connectDB from '../../../../utils/connect'
+import User from '@/models/userModel'
+import bcrypt from 'bcryptjs';
+
+
+const login = async (credentials) => {
+  try{
+    connectDB();
+    const user = await User.findOne({email: credentials.email});
+    if(!user) throw new Error('Wrong credentials')
+    const isCorrect = await bcrypt.compare(credentials.password, user.password);
+    if(!isCorrect) throw new Error('Wrong credentials')
+    return user;
+
+  }
+  catch(err){
+    
+    throw new Error('Something went wrong');
+  }
+}
 
 export const authOptions = {
   pages: {
@@ -11,16 +31,34 @@ export const authOptions = {
       credentials: {},
       async authorize(credentials){
         try{
-          console.log({ credentials })
-
+          const user = await login(credentials)
+          return user
         }
         catch(err){
-          console.log(err)
-          return null
+          throw new Error('Failed to login ');
         }
       }
     })
-  ]
+  ],
+  callbacks: {
+    async jwt(token, user){
+      if(user){
+        token.username = user.username;
+        token.email = user.email,
+        token.id = user.id
+      }
+      return token;
+    },
+    async session(session, token){
+      if(token){
+        session.user.username = token.username,
+        session.user.email = token.email,
+        session.user.id = token.id
+      }
+      return session;
+    }
+  
+  }
 }
 
 const handler = NextAuth(authOptions);
